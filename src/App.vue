21ba44fe9c8092a0c527c46f0a8e04ecda4f8be8
jason60810 <jason60810@gmail.com>
2021-12-09 09:15:36 +0800

    create

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1 class="header1 text-center" style="color: red">
+      id is {{ watersData.id }}
+    </h1>
+    <h1 class="header2 text-center" style="color: blue">
+      inputChar is {{ watersData.inputChar }}
+    </h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
+  name: 'App',
+  data() {
+    return {
+      watersData: {},
+    };
+  },
+  created() {
+    this.axios
+      .get('/api/calculator/test/5/')
+      .then((res) => {
+        console.log(res.data);
+        this.watersData = res.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   },
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss"></style>
---
+++ vue.config.js
@@ -0,0 +1,28 @@
+//vue.config.js
+module.exports = {
+  publicPath:
+    process.env.NODE_ENV === 'production'
+      ? '/test_api_3/' // test20200915 為 repo 名稱
+      : '/',
+  devServer: {
+    port: 8080,
+
+    host: 'localhost',
+
+    https: false,
+
+    // 自动启动浏览器
+
+    open: false,
+    proxy: {
+      '/api': {
+        target: 'https://warm-helix-333707.appspot.com/', //设置调用的接口域名和端口
+        changeOrigin: true, //是否跨域
+        ws: true,
+        pathRewrite: {
+          '^/api': '',
+        },
+      },
+    },
+  },
+};
