e03ac28a55cbea82b461ca4ddcdc0ac07598b2dd
jason60810 <jason60810@gmail.com>
2021-12-09 09:48:29 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -4,25 +4,25 @@ module.exports = {
     process.env.NODE_ENV === 'production'
       ? '/test_api_3/' // test20200915 為 repo 名稱
       : '/',
-  devServer: {
-    port: 8080,
+  // devServer: {
+  //   port: 8080,
 
-    host: 'localhost',
+  //   host: 'localhost',
 
-    https: false,
+  //   https: false,
 
-    // 自动启动浏览器
+  //   // 自动启动浏览器
 
-    open: false,
-    proxy: {
-      '/api': {
-        target: 'https://warm-helix-333707.appspot.com/', //设置调用的接口域名和端口
-        changeOrigin: true, //是否跨域
-        ws: true,
-        pathRewrite: {
-          '^/api': '',
-        },
-      },
-    },
-  },
+  //   open: false,
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://warm-helix-333707.appspot.com/', //设置调用的接口域名和端口
+  //       changeOrigin: true, //是否跨域
+  //       ws: true,
+  //       pathRewrite: {
+  //         '^/api': '',
+  //       },
+  //     },
+  //   },
+  // },
 };
